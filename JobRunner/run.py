48b5e20e0ba3d083c13ef318ccae16247cdc2393
Rob Willison <Rob@willison.co.uk>
2018-01-28 14:00:36 +0000

    more work

--- JobRunner/run.py
+++ JobRunner/run.py
@@ -4,7 +4,6 @@ from DataGatherer import RainGatherer
 from DataGatherer import RiverLevelGatherer
 from DataGatherer import RainForecastGatherer
 from DataGatherer import DarkSkyGatherer
-from Prediction import TrainingData
 from Prediction import Model
 from Prediction import Predict
 from AcuracyCalculator import Calculate
